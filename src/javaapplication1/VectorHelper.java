e24bc8c4dc5ccabb30b6c6ee000f39ca998eedf4
KarimBen96 <fk_ben_hocine@esi.dz>
2017-10-21 22:19:02 +0100

    very final merge1

--- src/javaapplication1/VectorHelper.java
+++ src/javaapplication1/VectorHelper.java
@@ -12,6 +12,145 @@ package javaapplication1;
 public class VectorHelper {
     
     
+    
+    /**
+     * trier le tableau tab
+     * @param tab 
+     * tableau de double a trier 
+     * @param taille la taille du tableau tab
+     */
+    public static void tabTrie(int tab[],int taille){ // ordoner les ele,ent d un tqble
+
+        int indice_max;
+
+        for(int i=taille;i>1;i--){
+
+            indice_max=max(tab,i);
+
+            echanger(tab,i-1,indice_max);
+
+        }
+
+    }
+
+    
+    
+    
+    
+    /**
+     * trouver le max dans le tableau 1D tab
+     * @param tab un tableau 1D
+     * @param taille la taille du tableau tab
+     * @return return l'indice du max dans tab
+     */
+    public static int max(int tab[],int taille){
+
+        int i=0,indice_max=0;
+
+        while(i<taille){
+
+            if(tab[i]>tab[indice_max])
+
+                indice_max=i;
+
+            i++;
+
+        }
+
+        return indice_max; 
+
+    }
+
+    
+    
+    /**
+     *permuter deux case du tableau tab
+     * @param tab un tableau a 1d
+     * @param i indice de la i eme case dans tab
+     * @param j indice de la j eme cas dans tab
+     */
+    public static void echanger(int tab[],int i,int j){
+
+        int tmp;
+
+        tmp=tab[i];
+
+        tab[i]=tab[j];
+
+        tab[j]=tmp;
+
+    }
+
+    
+    
+    
+    
+    /**
+     *
+     * @param tab le tableau a inverser .
+     * @param taille la taille du tableau tab
+     */
+    public static void show(int tab[],int taille)
+  {
+      for(int i=0;i<taille;i++)
+          System.out.println("tab["+i+"]="+tab[i]+"\n");
+  }
+    
+    
+    
+    
+    
+/** 
+ * 
+ *  inverser les element du tableau tab
+ * @param tab 
+ * le tableau a inverser
+ * 
+ * 
+ */
+   public static void inverse(int tab[])
+  {
+     int a;
+     for(int i=0,j=tab.length-1;i<tab.length/2;i++,j--)
+     {
+         a=tab[i];
+         tab[i]=tab[j];
+         tab[j]=a;
+     }
+  }
+
+   
+   
+   
+    /**
+     *
+     * @param tab1 tableau a 1D 
+     * @param tab2 Tableau a 1D
+     * @return return  un Tableau de 1D des Double de la meme taille que tab1 et tab2 contenant la somme des element de tab1 et tab2
+     *  
+     */
+    public static int[] somme(int tab1[],int tab2[]) 
+   {
+       int tabres[]= new int[tab1.length];
+       if(!(tab1.length==tab2.length) ) return null;
+       
+       else
+       {
+         for(int i=0;i<tab1.length;i++)   
+         {
+           tabres[i]=tab1[i]+tab2[i];  
+         }
+       }
+      
+       
+       show(tabres,tabres.length);
+       return tabres;
+   }
+    
+    
+    
+    
+    
     /**
      * getMinMax recupere le minimum et le maximum d'un tableau
      * 
@@ -36,6 +175,9 @@ public class VectorHelper {
         return vect;
     }
 
+    
+    
+    
     /**
      * formule multiplie tous les elements d'un tableau fois 2
      * 
--- test/javaapplication1/VectorHelperTest.java
+++ test/javaapplication1/VectorHelperTest.java
@@ -39,6 +39,120 @@ public class VectorHelperTest {
 
  
 
+    
+    /**
+    * Test of tabTrie method, of class VectorHelper.
+    */
+    @Test
+    public void testTabTrie() {
+        System.out.println("tabTrie");
+        int[] tab = {1,25,30,-30};
+        int[] restab = {-30,1,25,30};
+        int taille = 4;
+       
+        VectorHelper.tabTrie(tab, taille);
+       // assert.assertArrayEquals(restab, tab);
+        //assert.assertArrayEquals(restab, tab,);
+        assertArrayEquals(restab, tab);
+        
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    
+    
+    
+    /**
+     * Test of max method, of class VectorHelper.
+     */
+    @Test
+    public void testMax() {
+        System.out.println("max");
+        int[] tab = {12,25,30,15,1258};
+        int taille = 5;
+        int expResult = 4;
+        int result = VectorHelper.max(tab, taille);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    
+    
+    
+    /**
+     * Test of echanger method, of class VectorHelper.
+     */
+    @Test
+    public void testEchanger() {
+        System.out.println("echanger");
+        int[] result = {350,12,500,40,15,1};
+        int[] expResult = {350,12,500,40,15,1};
+        int i = 0;
+        int j = 0;
+        VectorHelper.echanger(result, i, j);
+        assertArrayEquals(expResult, result);
+        System.out.println("premier test marche");
+         i = 2;
+         j = 5;
+       int[] result1 = {350,12,500,40,15,1};
+        int[] expResult1 = {350,12,1,40,15,500};
+        VectorHelper.echanger(result1, i, j);
+        assertArrayEquals(expResult1, result1);
+        System.out.println("deuxieme test marche");
+
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+ 
+
+    /**
+     * Test of inverse method, of class VectorHelper.
+     */
+    @Test
+    public void testInverse() {
+        System.out.println("inverse");
+        int[] result = {1,15,40,500,12,350};
+        int[] expResult = {350,12,500,40,15,1};
+        VectorHelper.inverse(result);
+              assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    
+    
+    
+    /**
+    * Test of somme method, of class VectorHelper.
+    */
+    @Test
+    public void testSomme() throws Exception {
+        System.out.println("somme \n la meme taille");
+        int[] tab1 = {1,15,40};
+        int[] tab2 = {1,15,40};
+        int[] expResult ={2,30,80};
+        int[] result = VectorHelper.somme(tab1, tab2);
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+        System.out.println("somme \n la taille different");
+        int[] tab11 = {1,15,40};
+        int[] tab22 = {1,15,40,30};
+        int[] expResult2 =null;
+        int[] result2 = VectorHelper.somme(tab11, tab22);
+        assertArrayEquals(expResult2, result2);
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
     /**
      * Test of getMinMax2 method, of class VectorHelper.
      */
@@ -54,6 +168,10 @@ public class VectorHelperTest {
         //fail("The test case is a prototype.");
     }
 
+    
+    
+    
+    
     /**
      * Test of formule2 method, of class VectorHelper.
      */
